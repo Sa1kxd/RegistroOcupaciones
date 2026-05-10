0bc3a9a78bd89026cfc8f1b843712310d3c7d44b
Sa1kxd <samildelacruz1@gmail.com>
2026-05-10 14:59:29 -0400

    ✨: agregar OcupacionesEntity

---
+++ app/src/main/java/com/example/registroocupaciones/data/local/OcupacioneEntity.kt
@@ -0,0 +1,11 @@
+package com.example.registroocupaciones.data.local
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "ocupaciones")
+data class OcupacionEntity(
+    @PrimaryKey(autoGenerate = true)
+    val ocupacionId: Int = 0,
+    val descripcion: String,
+    val sueldo: Double
+)
