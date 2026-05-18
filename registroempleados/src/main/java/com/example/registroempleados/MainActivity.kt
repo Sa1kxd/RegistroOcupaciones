11f410eb75d11214f7d5fed76449040c7d9c2f71
Sa1kxd <samildelacruz1@gmail.com>
2026-05-18 13:27:23 -0400

    🔧: Ajustes necesarios para AndroidManifest.xml y MainActivity.kt

--- registroempleados/src/main/java/com/example/registroempleados/MainActivity.kt
+++ registroempleados/src/main/java/com/example/registroempleados/MainActivity.kt
@@ -5,21 +5,26 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.registroempleados.presentation.navigation.EmpleadoNavHost
 import com.example.registroempleados.ui.theme.RegistroOcupacionesTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             RegistroOcupacionesTheme {
-
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    EmpleadoNavHost()
+                }
                 }
             }
         }
